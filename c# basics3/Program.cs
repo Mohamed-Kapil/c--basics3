66b8d07388053c5f0d7b908750f6ca87b14cc8f2
kapil <mohamedkapil31@gmail.com>
2026-08-30 19:54:45 +0300

    Add project files.

---
+++ c# basics3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c__basics3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
